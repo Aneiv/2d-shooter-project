a8d37d4491f28ed6273bf035d295fe2585c89baa
Aneiv <161586104+Aneiv@users.noreply.github.com>
2025-07-10 16:53:52 +0200

    Added ingame fps counter

---
+++ Assets/Scripts/Manager/FPSDisplay.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+
+
+public class FPSDisplay : MonoBehaviour
+{
+    public TMP_Text fpsText;
+    float deltaTime = 0.0f;
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = string.Format("{0:0.}", fps);
+    }
+}

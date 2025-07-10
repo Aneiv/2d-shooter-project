c515ee212ac212eafc50230f6be10eaf79bfe60f
Aneiv <161586104+Aneiv@users.noreply.github.com>
2025-07-10 21:58:24 +0200

    Added enemy random wave style system
    Added new wave spawning when no enemies on screen with time delay

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,17 +5,17 @@ public class Enemy : MonoBehaviour, IShooter, IHealth
     public int hp = 50;
     public int bulletDamage = 10;
     public int BulletDamage => bulletDamage;
-
+    public GameObject waveManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        waveManager = GameObject.FindGameObjectWithTag("GameController");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void TakeDamage(int damage)
@@ -32,6 +32,9 @@ public class Enemy : MonoBehaviour, IShooter, IHealth
     }
     public void Die()
     {
+        //Debug.Log("KILLED ENEMY");
+        var destroyTrigger = waveManager.GetComponent<NextWaveTrigger>();
+        destroyTrigger.EnemyKilled();
         Destroy(gameObject);
     }
 }
